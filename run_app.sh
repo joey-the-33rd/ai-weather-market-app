49eafb9b978f0b2f265c7f38225e33e3494691b1
Joseph Ndungu <jothefu@gmail.com>
2025-04-16 03:41:17 +0300

    Create run_app.sh
    run_app.sh

---
+++ run_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to activate the virtual environment and run the Flask app
+
+# Activate the virtual environment
+source ai-weather-market-app/aiwma_env/bin/activate
+
+# Run the Flask app
+python app_server.py
